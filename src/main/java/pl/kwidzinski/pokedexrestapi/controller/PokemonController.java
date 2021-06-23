8f8963228b22110c9523b5be3c0c4f2b4f73e9cb
PawelKwidzinski <solvap83@gmail.com>
2021-06-23 13:14:11 +0200

    Created PokemonController on @RequestMapping("/pokemons") with methods: @GetMapping -> getAll(), @GetMapping("/{id}") - getById(), @PostMapping -> addPokemon() save(), @PutMapping("/{id}") -> updatePokemon(), @DeleteMapping("/{id}") -> delete()

---
+++ src/main/java/pl/kwidzinski/pokedexrestapi/controller/PokemonController.java
@@ -0,0 +1,64 @@
+package pl.kwidzinski.pokedexrestapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.kwidzinski.pokedexrestapi.model.Pokemon;
+import pl.kwidzinski.pokedexrestapi.service.PokemonService;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pokemons")
+public class PokemonController {
+
+    private final PokemonService pokemonService;
+
+    @Autowired
+    public PokemonController(final PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pokemon>> getAll() {
+        List<Pokemon> pokemonList = pokemonService.getAll();
+        return ResponseEntity.ok(pokemonList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pokemon> getById(@PathVariable ("id") Long pokemonId) {
+        Optional<Pokemon> optional = pokemonService.findById(pokemonId);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pokemon> add(@RequestBody @Valid Pokemon newPokemon) {
+        Pokemon result = pokemonService.save(newPokemon);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pokemon> update(@PathVariable("id") Long id, @RequestBody Pokemon pokemonToUpdate) {
+        if (pokemonService.findById(id).isPresent()) {
+            pokemonToUpdate.setId(id);
+            pokemonService.save(pokemonToUpdate);
+            return ResponseEntity.accepted().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Pokemon> delete(@PathVariable("id") Long idToDelete) {
+        if (pokemonService.existById(idToDelete)) {
+            pokemonService.delete(idToDelete);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
