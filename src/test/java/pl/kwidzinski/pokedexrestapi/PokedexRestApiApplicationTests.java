ffb86204470a3cc45453bb8e6adf24a24934e891
PawelKwidzinski <solvap83@gmail.com>
2021-06-23 11:59:25 +0200

    Initial commit

---
+++ src/test/java/pl/kwidzinski/pokedexrestapi/PokedexRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.pokedexrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
