0da96beef9e36adb5bbba7578e50aa7b8d3d8bcb
PawelKwidzinski <solvap83@gmail.com>
2021-06-23 13:14:40 +0200

    Initial Commit

---
+++ src/main/java/pl/kwidzinski/pokedexrestapi/PokedexRestApiApplication.java
@@ -0,0 +1,12 @@
+package pl.kwidzinski.pokedexrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexRestApiApplication.class, args);
+    }
+}
