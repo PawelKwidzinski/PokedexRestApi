eb690a2324380fbce97aa9eb07d099dd0335b27d
PawelKwidzinski <solvap83@gmail.com>
2021-06-24 18:42:51 +0200

    Added test data for test environment

---
+++ src/main/resources/test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO pokemon (name, species, type) VALUES
+('Pikachu', 'Mouse', 'ELECTRIC'),
+('Bulbasaur', 'Seed', 'POISON'),
+('Charmander', 'Lizard', 'FIRE'),
+('Squirtle', 'Turtle', 'WATER'),
+('Articuno', 'Freeze', 'ICE'),
+('Zapdos', 'Electric', 'ELECTRIC');
