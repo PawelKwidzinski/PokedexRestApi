c0e4151f6eb94da8960d5024e5c35cbb6f28e237
PawelKwidzinski <solvap83@gmail.com>
2021-06-24 21:55:23 +0200

    Deleted unnecessary exception

--- src/main/java/pl/kwidzinski/pokedexrestapi/exceptions/RestExceptionHandler.java
+++ src/main/java/pl/kwidzinski/pokedexrestapi/exceptions/RestExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import pl.kwidzinski.pokedexrestapi.exceptions.definition.EntityBadRequestException;
 import pl.kwidzinski.pokedexrestapi.exceptions.definition.EntityNotFoundException;
 import pl.kwidzinski.pokedexrestapi.exceptions.response.PokemonMessageException;
 
@@ -12,17 +11,6 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class RestExceptionHandler {
-
-    @ExceptionHandler(value = {EntityBadRequestException.class})
-    public ResponseEntity<Object> handlePokemonBadRequest(EntityBadRequestException ex) {
-        HttpStatus badRequestStatus = HttpStatus.BAD_REQUEST;
-        PokemonMessageException pokemonMessageException = new PokemonMessageException(
-                ex.getMessage(),
-                badRequestStatus,
-                LocalDateTime.now()
-        );
-        return new ResponseEntity<>(pokemonMessageException, badRequestStatus);
-    }
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
     public ResponseEntity<Object> handlePokemonNotFound(EntityNotFoundException ex) {
--- src/main/java/pl/kwidzinski/pokedexrestapi/exceptions/definition/EntityBadRequestException.java
+++
@@ -1,8 +0,0 @@
-package pl.kwidzinski.pokedexrestapi.exceptions.definition;
-
-public class EntityBadRequestException extends RuntimeException{
-
-    public EntityBadRequestException(final Long id) {
-        super("Could not find Entity Pokemon with id: " + id);
-    }
-}
